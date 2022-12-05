e3ae3df023680e265b8f7ba6f0c1ecb1bd6de7a5
AMichelazzo <47867706+AMichelazzo@users.noreply.github.com>
2022-12-05 11:43:15 +0000

    Initial commit

---
+++ product.h
@@ -0,0 +1,17 @@
+#ifndef PRODUCT_H
+#define PROFUCT_H
+
+#include <string>
+
+class Product {
+
+    protected:
+        std::string name;
+        double price;
+    
+    public:
+        
+}
+
+
+#endif
